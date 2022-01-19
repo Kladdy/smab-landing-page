f8624c034fa0a3d43bde39bb18728445ccd7486b
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-01-19 16:52:51 +0100

    Add flygmedicin and update publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 const upath = require('upath');
 const sh = require('shelljs');
 
+const destPathToRemove = upath.resolve(upath.dirname(__filename), '../docs');
+
+sh.rm('-rf', `${destPathToRemove}`)
+
 const sourcePath = upath.resolve(upath.dirname(__filename), '../dist/.');
-const destPath = upath.resolve(upath.dirname(__filename), '../docs/');
+const destPath = upath.resolve(upath.dirname(__filename), '../docs');
     
 sh.cp('-R', sourcePath, destPath)
