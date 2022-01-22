d05718aad2876b7c71ee6c2fb14848b98d68e3f1
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-01-22 02:32:10 +0100

    Make sure publish command copies over CNAME

--- scripts/publish.js
+++ scripts/publish.js
@@ -11,3 +11,9 @@ const sourcePath = upath.resolve(upath.dirname(__filename), '../dist/.');
 const destPath = upath.resolve(upath.dirname(__filename), '../docs');
     
 sh.cp('-R', sourcePath, destPath)
+
+// Copy over CNAME file aswell
+const sourcePathCNAME = upath.resolve(upath.dirname(__filename), '../src/CNAME');
+const destPathCNAME = upath.resolve(upath.dirname(__filename), '../docs');
+    
+sh.cp('-R', sourcePathCNAME, destPathCNAME)
